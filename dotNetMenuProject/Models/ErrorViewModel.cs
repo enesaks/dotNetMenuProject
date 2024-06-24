c0985050e61f862c401520ec8338623be6073ac9
Enes <98012557+enesaks@users.noreply.github.com>
2024-06-24 21:43:16 +0300

    Projenin oluşturulması ve Temanın eklenmesi.

---
+++ dotNetMenuProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace dotNetMenuProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+

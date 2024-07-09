72b58f28a58a7990f62d4c366709bf14e061dd8a
Enes <98012557+enesaks@users.noreply.github.com>
2024-07-09 22:57:44 +0300

    dotNetMenuProject
    Weather api ile kullanıcıdan alınan şehrin hava durumunu gösterme işlemi.

--- dotNetMenuProject/Controllers/WeatherController.cs
+++ dotNetMenuProject/Controllers/WeatherController.cs
@@ -6,35 +6,38 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Net.Http;
 using dotNetMenuProject.Models;
+
 namespace dotNetMenuProject.Controllers
 {
     public class WeatherController : Controller
     {
-       
-        public async Task<IActionResult> Index()
+        [HttpGet]
+        public IActionResult Index()
         {
-            
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetWeather(WeatherViewModel.Data model)
+        {
             var client = new HttpClient();
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Get,
-                RequestUri = new Uri("https://the-weather-api.p.rapidapi.com/api/weather/Ankara"),
+                RequestUri = new Uri($"https://the-weather-api.p.rapidapi.com/api/weather/{model.City}"),
                 Headers =
-    {
-        { "x-rapidapi-key", "9d15b0d74bmshf87a4ae058b2695p1c746bjsnfb94aa854e8f" },
-        { "x-rapidapi-host", "the-weather-api.p.rapidapi.com" },
-    },
+                {
+                    { "x-rapidapi-key", "9d15b0d74bmshf87a4ae058b2695p1c746bjsnfb94aa854e8f" },
+                    { "x-rapidapi-host", "the-weather-api.p.rapidapi.com" },
+                },
             };
             using (var response = await client.SendAsync(request))
             {
                 response.EnsureSuccessStatusCode();
                 var body = await response.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<WeatherViewModel>(body); //Jsonı metine çevirmek.
-                return View(values.data);
+                var values = JsonConvert.DeserializeObject<WeatherViewModel>(body);
+                return View("WeatherResult", values.data);
             }
-            
-            
         }
     }
 }
-
--- dotNetMenuProject/Models/WeatherViewModel.cs
+++ dotNetMenuProject/Models/WeatherViewModel.cs
@@ -3,13 +3,16 @@ namespace dotNetMenuProject.Models
 {
 	public class WeatherViewModel
 	{
+        
         public bool success { get; set; }
         public Data data { get; set; }
+         
 
         public class Data
         {
             public string temp { get; set; }
             public string expected_temp { get; set; }
+            public string City { get; set; }
         }
 
     }
